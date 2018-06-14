8d76d18389d0d3d6f0e4dc284c36671cf1865725
elseagle <dowolebolu@gmail.com>
2018-06-14 16:11:12 +0100

    now saves ocr to docx format

--- digitizer.py
+++ digitizer.py
@@ -1,15 +1,25 @@
 import cv2
-import sys
-from pytesseract.pytesseract import tesseract_cmd as cmd
+import sys, os, glob
+import pytesseract
 from pytesseract import image_to_string
+from docx import Document
+
+# import glob, os
+# os.chdir("/mydir")
+# for file in glob.glob("*.txt"):
+#     print(file)
 
 
 if __name__ == "__main__":
+    doc_file = Document()
     command_length = len(sys.argv) 
 
     if command_length < 2 :
         print ("Usage: python digitizer.py <image-name>.jpg")
-        sys.exit(1)
+        sys.exit(1)if not os.path.exists(directory):
+        #     print ("making directory")
+        #     os.makedirs(directory)
+        #     doc_file.save(file_path)
 
     image_path = sys.argv[1]
     cmd = "/usr/bin/tesseract"
@@ -21,7 +31,29 @@ if __name__ == "__main__":
     text = image_to_string(image, config=config)
 
     if text:
+        doc_file.add_paragraph(text)
+        list_text = list(text)
+        if len(text) >9:
+            list_name = list_text[:6]
+        else:
+            list_name = list_text[:]
+        name = ''.join(list_name)
+        file_path = "{}.docx".format(name)
+        # list_directory = list(file_path)
+        # list_directory_create = list_directory[:31]
+        # directory_create = "".join(list_directory_create)
+        # directory = os.path.dirname(directory_create)
+        # if not os.path.exists(directory):
+        #     print ("making directory")
+        #     os.makedirs(directory)
+        #     doc_file.save(file_path)
+        doc_file.save(file_path)
+
+                
+                    
+
         print(text)
+
     else:
         print("No text found. Make sure you have a good quality for your picture")
         
